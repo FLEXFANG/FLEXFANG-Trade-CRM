db3447ef2983e51c7e4dbc9ea21c61031c9857a3
FELXFANG <fangxinyu168@gmail.com>
2026-08-28 19:23:39 +0800

    feat(crm): expose trade customer detail query

--- yudao-module-crm/src/main/java/cn/iocoder/yudao/module/crm/service/trade/CrmTradeCustomerQueryService.java
+++ yudao-module-crm/src/main/java/cn/iocoder/yudao/module/crm/service/trade/CrmTradeCustomerQueryService.java
@@ -1,6 +1,7 @@
 package cn.iocoder.yudao.module.crm.service.trade;
 
 import cn.iocoder.yudao.framework.common.pojo.PageResult;
+import cn.iocoder.yudao.module.crm.controller.admin.trade.vo.CrmTradeCustomerDetailRespVO;
 import cn.iocoder.yudao.module.crm.controller.admin.trade.vo.CrmTradeCustomerPageReqVO;
 import cn.iocoder.yudao.module.crm.controller.admin.trade.vo.CrmTradeCustomerRespVO;
 
@@ -12,5 +13,7 @@ import cn.iocoder.yudao.module.crm.controller.admin.trade.vo.CrmTradeCustomerRes
 public interface CrmTradeCustomerQueryService {
 
     PageResult<CrmTradeCustomerRespVO> getTradeCustomerPage(CrmTradeCustomerPageReqVO reqVO, Long userId);
+
+    CrmTradeCustomerDetailRespVO getTradeCustomerDetail(Long customerId);
 
 }
